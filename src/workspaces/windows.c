8e674bc4ce67bedeaed0ddbd0ef463fc0973fe48
niljimeno <niliara@proton.me>
2026-01-08 10:35:49 +0100

    make proper floating checks

--- src/workspaces/windows.c
+++ src/workspaces/windows.c
@@ -55,7 +55,6 @@ struct WindowData initialiseWindow(Window w) {
     /* check for unusal types */
     if (XGetWindowProperty(display, w, _NET_WM_WINDOW_TYPE, 0, 8, False, XA_ATOM, &type, &format,
                            &n_items, &after, (unsigned char **)&types) == Success && types) {
-
         for (unsigned long i = 0; i < n_items; i++) {
             if (types[i] == _NET_WM_WINDOW_TYPE_DOCK) {
                 XFree(types);
@@ -89,38 +88,14 @@ struct WindowData initialiseWindow(Window w) {
         winData.shouldFloat = True;
     }
 
-    Atom net_wm_allowed_actions = XInternAtom(display, "_NET_WM_ALLOWED_ACTIONS", False);
-    Atom actual_type;
-    int actual_format;
-    unsigned long nitems, bytes_after;
-    unsigned char *data;
-
-    // check i resizing is allowed??? i think??
-    if (!winData.shouldFloat) {
-    if (XGetWindowProperty(display, w, net_wm_allowed_actions, 0, 1024, False, AnyPropertyType,
-            &actual_type, &actual_format, &nitems, &bytes_after, &data) == Success) {
-        if (actual_type != None && data != NULL) {
-            Atom *actions = (Atom *)data;
-            for (unsigned long i = 0; i < nitems; i++) {
-                if (actions[i] == XInternAtom(display, "_NET_WM_ACTION_RESIZE", False)) {
-                    winData.shouldFloat = True;
-                    XFree(data);
-                    return winData;
-                }
-            }
-        }
-        XFree(data);
-    }}
+    XWindowAttributes wa;
+    XGetWindowAttributes(display, w, &wa);
+    if (wa.override_redirect) winData.shouldFloat = True;
 
     XClassHint *class_hint = XAllocClassHint();
     if (XGetClassHint(display, w, class_hint)) {
         printf("Window class: %s\n", class_hint->res_class);
         printf("Window name: %s\n", class_hint->res_name);
-        if (strcmp(class_hint->res_name, "dmenu") == 0) {
-            winData.shouldFloat = True;
-        } else if (strcmp(class_hint->res_name, "libresprite") == 0) {
-            winData.shouldFloat = True;
-        }
     } else {
         printf("Failed to get window class hint\n");
     }
