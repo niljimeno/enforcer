eed6bf0a4be61a55ecb3e5a7da84b8f9d3496771
niljimeno <niliara@proton.me>
2026-01-19 17:36:08 +0100

    fix freezes due to XSync

--- src/triggers/handlers.c
+++ src/triggers/handlers.c
@@ -6,7 +6,8 @@ void handleKeyPress(XEvent *ev) {
     KeyCode code = ev->xkey.keycode;
     if (code == XKeysymToKeycode(display, XStringToKeysym("Return"))) {
         spawnDaemon("st");
-    } else if (code == XKeysymToKeycode(display, XStringToKeysym("q")) && ev->xkey.state & ShiftMask) {
+        printf("open new window\n");
+    } else if (code == XKeysymToKeycode(display, XStringToKeysym("c")) && ev->xkey.state & ShiftMask) {
         spawnDaemon("pkill -f xinit");
     } else if (code == XKeysymToKeycode(display, XStringToKeysym("p"))) {
         sh("dmenu_run");
@@ -41,7 +42,6 @@ void handleMapNotify(XEvent *ev) {
 }
 
 void handleDestroyNotify(XEvent *ev) {
-    XSync(display, False);
     Window w = ev->xdestroywindow.window;
     if (w && nodeExists(w)) {
         removeNode(w);
